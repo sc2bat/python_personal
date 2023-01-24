f9430a9d9271c9461a3fcbd16de2bf970ddc912d
sc2bat <izar_dero@naver.com>
2023-01-25 07:05:55 +0900

    while for

---
+++ lesson_003/002_while.py
@@ -0,0 +1,48 @@
+'''
+# while 
+print('a >= 0 running')
+
+a = int(input('a : '))
+
+while a >= 0 :
+    print('running')
+    a = int(input('a : '))
+'''
+
+'''
+# unlimited loop
+a = 10
+while a > 0 :
+    print('running a : ', a)
+'''
+
+'''
+# n번 반복
+n = int(input('n : '))
+while n :
+    print('n : ', n)
+    n -= 1
+'''
+
+'''
+# yes 입력시 반복
+text = 'y'
+while text == 'y' :
+    text = input('yes : ')
+print('end')
+'''
+
+'''
+# e 또는 E 가 입력될 때까지 반복하기
+text = input('e 또는 E 가 입력될 때까지 반복')
+while text != 'e' and text != 'E' :
+    text = input('e 또는 E 가 입력될 때까지 반복')
+print('end')
+'''
+
+
+
+
+
+
+
